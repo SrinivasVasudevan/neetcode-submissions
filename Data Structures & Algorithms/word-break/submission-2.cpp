6c188a8dfe1ca578c27be7db33498fe47faa1cf3
Srinivas Vasudevan <68465037+SrinivasVasudevan@users.noreply.github.com>
2026-06-14 18:14:37 -0400

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.length()+1, false);
+        unordered_set<string> wordSet;
+        unordered_set<int> wordLenSet;
+        dp[s.length()]=true;
+        for(int i=0;i<wordDict.size();i++){
+            wordSet.insert(wordDict[i]);
+            wordLenSet.insert(wordDict[i].length());
+        }
+
+        for(int i=s.length()-1;i>=0;i--){
+            for(int wL:wordLenSet){
+                if(i+wL<=s.length()){
+                    string temp = s.substr(i, wL);
+                    if(wordSet.count(temp)){
+                        dp[i]=dp[i+wL]||dp[i];
+                    }
+                    if(dp[i]) break;
+                }
+            }    
+        }
+
+        return dp[0];
+    }
+};
