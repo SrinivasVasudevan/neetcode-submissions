8355edd70b000995d1a9549157e2b45e135e6f96
Srinivas Vasudevan <68465037+SrinivasVasudevan@users.noreply.github.com>
2026-06-28 20:00:05 -0400

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool rZeros = false, cZeros = false;
+        for(int i=0;i<matrix[0].size();i++){
+            if(matrix[0][i] == 0) cZeros = true;
+        }
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i][0] == 0) rZeros=true;
+        }
+
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for(int i=matrix.size()-1;i>=0;i--){
+            for(int j=matrix[0].size()-1;j>=0;j--){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(rZeros){
+            for(int i=0;i<matrix.size();i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(cZeros){
+            for(int i=0;i<matrix[0].size();i++){
+                matrix[0][i]=0;
+            }
+        }
+    }
+};
