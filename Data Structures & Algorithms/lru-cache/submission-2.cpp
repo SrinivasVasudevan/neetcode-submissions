fe7178c51f486ebd7da47bdf734d5e516342373a
Srinivas Vasudevan <68465037+SrinivasVasudevan@users.noreply.github.com>
2026-07-19 21:36:52 -0400

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.cpp
@@ -0,0 +1,85 @@
+class LRUCache {
+public:
+    int capacity;
+    
+    class Node{
+        public:
+        int key;
+        int value;
+        Node* next;
+        Node* prev;
+
+        Node(int k, int v): key(k), value(v){
+            next=nullptr;
+            prev=nullptr;
+        }
+    };
+
+    Node* front;
+    Node* back;
+
+    unordered_map<int, Node*> mp;
+
+    LRUCache(int capacity): capacity(capacity) {
+        front = new Node(-1, -1);
+        back = new Node(-1, -1);
+        front->next=back;
+        back->prev=front;
+    }
+
+    Node* remove_key(int key){
+        Node* prev = front;
+        Node* curr = front->next;
+        Node* nxt;
+
+        while(curr){
+            nxt = curr->next;
+            if(curr->key == key){
+                break;
+            }
+            prev = curr;
+            curr = nxt;
+        }
+        
+        prev->next = nxt;
+        nxt->prev = prev;
+        return curr;
+    }
+
+    void insert_node(Node* node){
+        Node* bback = back->prev;
+        bback->next = node;
+        node->prev = bback;
+
+        node->next = back;
+        back->prev = node;
+    }
+    
+    int get(int key) {
+        if(mp.count(key)){
+            Node* rk = remove_key(key);
+            insert_node(rk);
+            return rk->value;
+        }else{
+            return -1;
+        }
+    }
+    
+    void put(int key, int value) {
+        if(mp.count(key)){
+            Node* rk = remove_key(key);
+            rk->value = value; 
+            insert_node(rk);
+            return;
+        }else{
+            if(mp.size() == capacity){
+                Node* rk = remove_key(front->next->key);
+                mp.erase(rk->key);
+                //delete rk;
+            }
+            Node* newnode = new Node(key, value);
+            mp[key] = newnode;
+            insert_node(newnode);
+        }
+    }
+};
